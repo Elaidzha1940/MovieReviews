4c922c9b583d8abda5fa06e57ee1215213de276f
Elaidzha1940 <eli080293@gmail.com>
2023-11-30 21:47:48 +0300

    MovieReviews

---
+++ MovieReviews/MovieReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieReviewsApp.swift
+//  MovieReviews
+//
+//  Created by Elaidzha Shchukin on 30.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
